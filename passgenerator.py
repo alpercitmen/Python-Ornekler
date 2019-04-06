99570a9348d5279a80a72fa275bf13e49285e843
Alper Çitmen <alper.citmen@gmail.com>
2019-04-06 16:45:43 +0300

    Password Generator {Python3}

---
+++ passgenerator.py
@@ -0,0 +1,12 @@
+#Python password generator
+
+import string
+from random import *
+
+characters = string.ascii_letters \
+    + string.punctuation \
+    + string.digits
+
+password = "".join(choice(characters) for x in range(randint(8, 16)))
+
+print(password)
